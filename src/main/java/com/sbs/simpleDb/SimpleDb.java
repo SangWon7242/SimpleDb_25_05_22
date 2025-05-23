1766dfb921abceaf4a650fe082555107824c9bb5
SangWon7242 <gotkdqja@gmail.com>
2025-05-23 19:46:56 +0900

    JDBC 연결 테스트

--- src/main/java/com/sbs/simpleDb/SimpleDb.java
+++ src/main/java/com/sbs/simpleDb/SimpleDb.java
@@ -1,11 +1,48 @@
 package com.sbs.simpleDb;
 
-public class SimpleDb {
-  public SimpleDb(String host, String username, String password, String dbName) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class SimpleDb {
+  private Connection conn;
+
+  public SimpleDb(String host, String username, String password, String dbName) {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      String url = "jdbc:mysql://%s:3306/%s?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull".formatted(host, dbName);
+
+      conn = DriverManager.getConnection(url, username, password);
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException("MySQL JDBC 드라이버를 찾을 수 없습니다.", e);
+    } catch (SQLException e) {
+      System.out.println("DB 연결 실패!");
+      System.out.println("에러 메시지: " + e.getMessage());
+      throw new RuntimeException("DB 연결 실패!", e);
+    }
   }
 
   public void setDevMode(boolean mode) {
 
+  }
+
+  public boolean isConnected() {
+    try {
+      return conn != null && !conn.isClosed();
+    } catch (SQLException e) {
+      return false;
+    }
+  }
+
+  public void close() {
+    if (conn != null) {
+      try {
+        conn.close();
+        System.out.println("DB 연결이 종료되었습니다.");
+      } catch (SQLException e) {
+        System.out.println("DB 연결 종료 중 오류 발생");
+        e.printStackTrace();
+      }
+    }
   }
 }
--- src/test/java/com/sbs/simpleDb/SimpleDbTest.java
+++ src/test/java/com/sbs/simpleDb/SimpleDbTest.java
@@ -1,9 +1,8 @@
 package com.sbs.simpleDb;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 @TestInstance(PER_CLASS)
@@ -14,10 +13,21 @@ public class SimpleDbTest {
   @BeforeAll
   public void beforeAll() {
     simpleDb = new SimpleDb("localhost", "root", "", "simpleDb_test");
+    simpleDb.setDevMode(true);
   }
 
   @Test
+  @DisplayName("t1: 데이터 베이스 연결 테스트")
   public void t1() {
+    assertTrue(simpleDb.isConnected(), "데이터베이스 연결이 실패했습니다.");
+    System.out.println("DB 연결 성공");
+  }
 
+  // 테스트 종료 후 연결 종료를 위한 메서드
+  @AfterAll
+  public void afterAll() {
+    if (simpleDb != null) {
+      simpleDb.close();
+    }
   }
 }
